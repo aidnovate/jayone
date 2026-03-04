a161c222d92632904c871882bd4c8799550cb183
Musah Razak Aidoo <67350960+aidoomusahrazal@users.noreply.github.com>
2026-03-04 13:13:21 +0000

    fixed

--- app/(dashboard)/dashboard/blogs/page.tsx
+++ app/(dashboard)/dashboard/blogs/page.tsx
@@ -1,7 +1,6 @@
 import DashboardLayout from '../components/Layout/DashboardLayout';
-import React from 'react';
-
 import BlogTable from '../components/BlogTable/BlogTable';
+
 export default function BlogsPage() { 
   return (
     <DashboardLayout active="Blogs">
